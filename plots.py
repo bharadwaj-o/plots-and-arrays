575bcdb41a9055c98884115ce8d756541b4451e2
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-29 13:04:13 +0530

    Update plots.py

--- plots.py
+++ plots.py
@@ -51,6 +51,21 @@ plt.show()
 
 
 
+x=np.array([80,85,90,95,100,105,110,115,120,125])
+y=np.array([240,250,260,270,280,290,300,310,320,330])
+
+plt.plot(x,y)
+plt.title("Smart Watch data")
+plt.xlabel("Avg Heartbeat")
+plt.ylabel("Calories consumed")
+
+plt.show()
+
+
+
+
+
+
 
 
 
