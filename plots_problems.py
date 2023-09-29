e0e1620b32aba4c371ace1b9508542dca665765f
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-29 16:39:45 +0530

    Update plots_problems.py

--- plots_problems.py
+++ plots_problems.py
@@ -6,8 +6,13 @@ plt.axhline(y=2, color='red')
 plt.axvline(x=2, color= 'red')
 plt.show()
 
+x=np.array(['A','B','C','D'])
+y=np.array([5,6,3,1])
 
-
+fig, ax = plt.subplots()
+z=ax.bar(x,y, 0.6)
+ax.bar_label(z, label_type='center')
+plt.show()
 
 
 
