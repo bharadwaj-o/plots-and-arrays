d821204f6cd9ca201c8ec754422e88ded29a81a3
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-29 15:51:06 +0530

    Update plots.py

--- plots.py
+++ plots.py
@@ -4,39 +4,39 @@ xpoints=np.array([1,8])
 ypoints=np.array([3,10])
 
 plt.plot(xpoints,ypoints, 'o')
-plt.show()
+
 
 xpoints=np.array([1,2,6,8])
 ypoints=np.array([3,8,1,10])
 
 plt.plot(xpoints,ypoints, 'o')
-plt.show()
+
 
 ypoints=np.array([3,8,1,10,5,7])
 
 plt.plot(ypoints)
-plt.show()
+
 
 plt.plot(ypoints, marker = 'o')
-plt.show()
+
 
 plt.plot(ypoints, marker='D')
-plt.show()
+
 
 plt.plot(ypoints, 'o-.k')
-plt.show()
+
 
 plt.plot(ypoints, 'o:g', ms =10,mec='k', mfc = 'w')
-plt.show()
+
 
 plt.plot(ypoints, linestyle = 'dashed')
-plt.show()
+
 
 plt.plot(ypoints, ls=':')
-plt.show()
+
 
 plt.plot(ypoints, linestyle= 'dashed', color='r')
-plt.show()
+
 
 
 ypoints1=np.array([3,8,1,10])
@@ -46,7 +46,7 @@ ypoints3=np.array([1,3,5,7])
 plt.plot(ypoints1)
 plt.plot(ypoints2)
 plt.plot(ypoints3)
-plt.show()
+
 
 
 
