80d0373f73fa6d403192af978ec85d00e1e7f4dc
bharadwaj-o <145960166+bharadwaj-o@users.noreply.github.com>
2023-09-29 17:14:01 +0530

    Update plots_problems.py

--- plots_problems.py
+++ plots_problems.py
@@ -15,6 +15,30 @@ ax.bar_label(z, label_type='center')
 plt.show()
 
 
+mass=np.array([10,20,30,40,50])
+weight=np.array([100,200,300,400,500])
+
+plt.plot(mass,weight, color='blue',linestyle='-')
+plt.xlabel("Mass")
+plt.ylabel("Weight")
+plt.title("Weight and Mass on Earth")
+plt.show()
+
+x=np.array([])
+y=np.array([])
+with open('test.txt', 'r') as testfile:
+    for i in testfile:
+        x=np.append(x, int(i[0]))
+        y=np.append(y, int(i[2]))
+plt.plot(x,y)
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("Sample Graph!")
+plt.show()
+
+
+
+
 
 
 
